d5d7086568c8f4fb66e9dab2554733948a552150
Andrew Ribicki <aribicki@saintjoe.edu>
2015-03-30 12:09:25 -0500

    my array for the array.js

---
+++ myarraynumbers.js
@@ -0,0 +1,10 @@
+var MYARRAY_FILE = "[\n " + [
+'{"randomnumber":"10"}',
+'{"randomnumber":"25"}',
+'{"randomnumber":"41"}',
+'{"randomnumber":"99"}',
+'{"randomnumber":"100"}',
+'{"randomnumber":"52"}',
+'{"randomnumber":"60"}',
+'{"randomnumber":"75"}',
+].join(",\n") + "\n]";
