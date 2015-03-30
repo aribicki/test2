cedf3e330048ec573bf4c2963788b67e8256739e
Andrew Ribicki <aribicki@saintjoe.edu>
2015-03-30 11:34:06 -0500

    # 2 showRegex.js  4points

---
+++ showRegex.js
@@ -0,0 +1,12 @@
+// CMP237 Exam 2 --- Andrew Ribicki 
+
+//var reg = /----/; 
+//var reg = new RegExp(“     “);
+// These are the two different ways to write them
+
+ verify(/ca?/,
+    ["cat", "Calm"]
+    
+  var reg = new RegExp("dog");
+      verify("dog");
+      
