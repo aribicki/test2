2785e3aa35822a227a0b5f320a474e59284dddca
Andrew Ribicki <aribicki@saintjoe.edu>
2015-03-30 12:53:24 -0500

    added comments

--- regexMatch.js
+++ regexMatch.js
@@ -1,6 +1,6 @@
 var str = 'ABCDEFGHIJKabcdefghijk ;
 var regexp = /w/;
-var mat_array = str.match(regexp) ;
+var mat_array = str.match(regexp) ; // not sure if this is what you wanted?
 console.log(mat_array) ;
 
 // will return all the letters seperated 
